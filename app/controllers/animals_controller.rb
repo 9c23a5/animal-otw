2b3064a59b9389fbaca9652201f30179ebf13893
9c23a5 <40715125+9c23a5@users.noreply.github.com>
2023-12-25 00:44:02 +0100

    Add simple view and scaffold controller

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class AnimalsController < ApplicationController
+  def index
+    # Gets the animal of the week
+  end
+
+  def show
+    # Gets an animal by ID
+  end
+end
